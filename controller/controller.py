8aa79a3f62e7179498a237fa7ee9564829e6d03c
fedotarte <artyom.fedotov93@gmail.com>
2021-06-03 22:58:44 +0300

    added requirements to pip install -r requirements.txt

--- controller/controller.py
+++ controller/controller.py
@@ -3,6 +3,8 @@ import sys
 import time
 from flask import Flask, Response, request, abort, send_file, session, jsonify, make_response
 from PIL import Image
+from flask_cors import CORS, cross_origin
+
 from helper.helpers import generate, validate, get_forecast
 
 
@@ -20,27 +22,31 @@ def create_logger(level=logging.INFO):
 
 log = create_logger(level=logging.INFO)
 app = Flask(__name__)
+CORS(app)
+
 
 @app.route('/calculate', methods=['POST'])
+@cross_origin()
 def calculate_forecast():
     content = request.json
     try:
         start_date = content['startDate']
         end_date = content['endDate']
         ticker = content['ticker']
+        interval = content['interval']
         epochs = content['epochs']
         batch_size = content['batchSize']
     except KeyError as e:
         bad_request = make_response('Bad request, check the fields: %s' % str(e), 400)
         return bad_request
-    check = get_forecast()
+    # check = get_forecast(start_date, end_date, ticker, interval, batch_size)
     return jsonify(
-        filename="sheeeit filename!"
+        imageId="monro.png",
     )
 
 
-
 @app.route('/getImage', methods=['GET'])
+
 def send_image():
     # content = request.json
     # print(content)
@@ -56,11 +62,14 @@ def send_image():
     # saved_file_name = generate()
     # yield img.save('%s.png' % saved_file_name)
     # filename = '%s.png' % saved_file_name
-    return send_file(sys.path[1] + '/savedfile_0x386cc4f0.png', mimetype='image/png')
+    image_id = request.args.get('imageId')
+    response = make_response(send_file(sys.path[1] + '/' + image_id, mimetype='image/png'))
+    response.headers.add("Access-Control-Allow-Origin", "*")
+    return response
+
 
 @app.after_request
 def after_request_func(response):
-
     """
     This function will run after a request, as long as no exceptions occur.
     It must take and return the same parameter - an instance of response_class.
